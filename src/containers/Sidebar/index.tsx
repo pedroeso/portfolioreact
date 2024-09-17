bf9daf239954d2b91440fd6b0e8775dd8f9e46c9
Pedro Ribeiro <pedrorocha@gmail.com>
2024-09-17 18:08:19 -0300

    Sua mensagem de commit aqui

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/pedroeso.png" />
+    <Titulo fontSize={20}>Pedro Ribeiro</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste obcaecati
+      nobis corrupti neque dignissimos molestias vitae illo minima, veniam
+      aspernatur! Incidunt aut possimus quod velit ullam, recusandae expedita
+      quos? Accusamus.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
