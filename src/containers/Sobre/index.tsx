7297cc54613345c14da7ea102505a0eba879ec7e
Pedro Ribeiro <pedrorocha@gmail.com>
2024-09-24 17:24:17 -0300

    construindo a estrutura e a seção "sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       aspernatur! Incidunt aut possimus quod velit ullam, recusandae expedita
       quos? Accusamus.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=pedroeso&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=pedroeso&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
