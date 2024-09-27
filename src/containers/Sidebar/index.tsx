ed2915e6642193f46e952e8d20398a7bfa7a0377
Pedro Ribeiro <pedrorocha@gmail.com>
2024-09-27 16:58:12 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/pedroeso.png" />
-    <Titulo fontSize={20}>Pedro Ribeiro</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Pedro Ribeiro</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        pedroeso
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
