263e73b45ccad4476d5e980a16aaa1000f770b42
Pedro Ribeiro <pedrorocha@gmail.com>
2024-10-01 18:52:58 -0300

    finalizando

--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const FotoPerfil = styled.img`
-  max-width: 100%;
+  width: 126px;
+  height: 128px;
   border-radius: 50%;
   margin-bottom: 24px;
 `
---
+++ src/themes/dark.ts
@@ -0,0 +1,17 @@
+const temaDark = {
+  corPrincipal: '#fff',
+  corSecundaria: '#eee',
+  corDeFunda: '#282a35',
+  corDeFundoBotao: '#fff',
+  corDaBorda: '#c1c1c1'
+}
+
+export default temaDark
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+  corDeFunda: string
+  corDeFundoBotao: string
+  corDaBorda: string
+}
